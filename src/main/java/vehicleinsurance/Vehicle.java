3abdbd935a49142676aa591e3f44d19463fa4f92
melcem <melcem@gmail.com>
2023-01-18 14:15:02 +0300

    "first commit"

---
+++ src/main/java/vehicleinsurance/Vehicle.java
@@ -0,0 +1,13 @@
+package vehicleinsurance;
+/*       1st term: June 2022     2nd term: December 2022
+             cars: 2000              cars: 2500
+            truck: 3000             truck: 3500
+        bus type1: 4000         bus type1: 4500
+        bus type2: 5000         bus type2: 5500
+       motorcycle: 1500        motorcycle: 1750
+    In case of an incorrect entry, the calculation failed warning and the menu should be shown again.
+    Provide an option to make a new transaction.
+ */
+public class Vehicle {
+
+}
---
+++ src/main/java/vehicleinsurance/VehicleInsurance.java
@@ -0,0 +1,16 @@
+package vehicleinsurance;
+/*  Vehicle Insurance Premium Calculator
+    Vehicle types: car, truck, bus, motorcycle.
+          -bus: 18-30 seats or 31 and above seats
+    Tariff period: December 2022, June 2022
+         1st term: June 2022     2nd term: December 2022
+             cars: 2000              cars: 2500
+            truck: 3000             truck: 3500
+        bus type1: 4000         bus type1: 4500
+        bus type2: 5000         bus type2: 5500
+       motorcycle: 1500        motorcycle: 1750
+    In case of an incorrect entry, the calculation failed warning and the menu should be shown again.
+    Provide an option to make a new transaction.
+ */
+public class VehicleInsurance {
+}
